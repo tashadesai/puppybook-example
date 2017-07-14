c783830a47b6fd2bac3bdf3419085ad3709fc33a
Sam Bakkila <sbakkila@gmail.com>
2017-07-14 15:57:54 -0400

    fixed single puppy

--- browser/components/SinglePuppy.js
+++ browser/components/SinglePuppy.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-export default class SinglePuppy extends Component {
+/* ----- COMPONENT ---- */
 
-  render () {
+class SinglePuppy extends Component {
+
+  componentDidMount() {
+    //Note this check in the componentDidMount. Ask yourself: Why is it used here? Hint: When is it important for the SinglePuppy component to run getPuppy, and when it is not?
+    if (!this.props.chosenPuppy.keys) {
+      const puppyId = this.props.match.params.puppyId
+      this.props.getPuppy(puppyId)
+    }
+  }
+
+  render() {
     return (
       <div>
-        <h2>PUPPY NAME GOES HERE</h2>
+        <h2>{this.props.chosenPuppy.name}</h2>
         <div>
-          <img src="http://placehold.it/400x400" />
+          <img src={this.props.chosenPuppy.image} />
         </div>
       </div>
     )
   }
 }
+/* ---------- CONTAINER -------- */
+
+import { connect } from 'react-redux'
+import { getPuppy } from '../redux/singlePuppy-actions'
+
+const mapStateToProps = (state) => ({ chosenPuppy: state.chosenPuppy })
+
+const mapDispatchToProps = { getPuppy }
+
+export default connect(mapStateToProps, mapDispatchToProps)(SinglePuppy)
